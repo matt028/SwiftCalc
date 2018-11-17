b5802f7a0daa4af98f4001aad4b38676c55f0f13
Matt Sutton <mattsuttondev@gmail.com>
2018-11-17 16:53:15 -0500

    SwiftCalc: Simple calculator app created in Swift.

---
+++ SwiftCalc/Controller/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  SwiftCalc
+//
+//  Created by Matthew Sutton on 10/28/18.
+//  Copyright © 2018 Matthew Sutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber = true
+    private var calculator = CalculatorLogic()
+    
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+        
+    }
+    
+    
+    
+    
+}
+
---
+++ SwiftCalc/Model/CalculatorLogic.swift
@@ -0,0 +1,61 @@
+//
+//  CalculatorLogic.swift
+//  SwiftCalc
+//
+//  Created by Matthew Sutton on 10/29/18.
+//  Copyright © 2018 Matthew Sutton. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+        
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n / 100
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
+        
+        return nil
+    }
+    
+}
